050200073373dc2dc8ef976778eef4314babfcad
ckchrisc <ckchrisc9306@gmail.com>
2026-06-16 10:36:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/handrip-coffee/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
